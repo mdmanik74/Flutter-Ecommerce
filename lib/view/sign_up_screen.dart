df4a7d1d66fc534fc216360cdeced02f7dfc4481
mdmanik74 <manikmhamud74@outlook.com>
2023-07-12 19:07:42 +0600

    sign up page

--- lib/view/sign_up_screen.dart
+++ lib/view/sign_up_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+import '../widget/custom_textform.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -8,13 +11,137 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  bool obscure = true;
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            body: Text(
-      'Welcome Back Sign Up',
-      style: Theme.of(context).textTheme.displayLarge,
-    )));
+          body: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Form(
+                          key: key,
+                          child: Column(children: [
+                            Text(
+                              'Welcome!',
+                              style: Theme.of(context).textTheme.displayLarge,
+                            ),
+                            Text(
+                              'Please Enter Your Account Here',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 30),
+                              child: CustomTextForm(
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return "Please Enter the email";
+                                  }
+                                  return null;
+                                },
+                                hint: 'Email or phone number',
+                                prefixIcon: IconlyBroken.message,
+                                obscureText: false,
+                                onTapSuffixIcon: () {},
+                              ),
+                            ),
+                            CustomTextForm(
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Please Enter the password";
+                                }
+                                return null;
+                              },
+                              obscureText: obscure,
+                              hint: 'Password',
+                              prefixIcon: IconlyBroken.lock,
+                              suffixIcon: obscure == true
+                                  ? IconlyBroken.show
+                                  : IconlyBroken.hide,
+                              onTapSuffixIcon: () {
+                                setState(() {
+                                  obscure = !obscure;
+                                });
+                              },
+                            ),
+                            Expanded(child: Column(
+                              children: [
+                                Row(children: const [Text('Your Password must Cotain')],
+
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(children:  [
+                                  const CircleAvatar(
+                                    radius: 10,
+                                    child: Icon(Icons.done,
+                                    size: 12,
+                                      color: Colors.cyan,
+                                    ),
+                                  ),
+                                  Text(' Must be 6 Character',
+                                    style: Theme.of(context).textTheme.bodyMedium,),
+                                ],),
+                                SizedBox(height: 10,),
+                                Row(children:  [
+                                  const CircleAvatar(
+                                    radius: 10,
+                                    child: Icon(Icons.done,
+                                      size: 12,
+                                      color: Colors.cyan,
+                                    ),
+                                  ),
+                                  Text(' Contains a Number',
+                                    style: Theme.of(context).textTheme.bodyMedium,),
+                                ],),
+                              ],
+                            ),
+                            ),
+
+                         Container(
+                                alignment: Alignment.center,
+                                margin: const EdgeInsets.symmetric(horizontal: 24),
+                                height: 56,
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlue,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: InkWell(
+                                  child: const Text(
+                                    'Sign Up',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Inter',
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 16),
+                                  ),
+                                  onTap: () {
+                                    setState(() {
+                                      key.currentState!.validate();
+                                    });
+                                  },
+                                ),
+                              ),
+
+
+                          ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
